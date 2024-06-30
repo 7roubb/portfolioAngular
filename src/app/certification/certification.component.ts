7bdfb95bc21b31f3adcebd2a63c402060452fcaf
7roubb <7roubb@gmail.com>
2024-06-30 19:57:20 +0300

    new update

--- src/app/certification/certification.component.ts
+++ src/app/certification/certification.component.ts
@@ -20,6 +20,19 @@ export class CertificationComponent {
       description: 'This certification covers fundamental concepts of JavaScript.',
       issuer: 'Coursera',
       date: 'January 12 2024'
+    }, {
+      imgPath:'../../assets/DataEng.jpeg',
+      name: 'Data Engineer Associate Certification',
+      description: 'This certification validates my knowledge, skills, and abilities required for an entry-level data engineer role',
+      issuer: 'Datacamp',
+      date: 'June 20 2024'
+    },
+    {
+      imgPath:'../../assets/SQLCert.png',
+      name: 'SQL Associate Certification',
+      description: 'Associate SQL Certificate program is designed to provide you with a comprehensive understanding of SQL',
+      issuer: 'Datacamp',
+      date: 'April 22 2024'
     },
     // Add more certifications as needed
   ];
